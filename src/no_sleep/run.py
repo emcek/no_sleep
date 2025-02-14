d91301cc52432d99b6ff9e089ee9e51a7bf395c0
Michał Plichta <michal.plichta@pl.abb.com>
2025-02-14 10:47:13 +0100

    use ArgumentParser

--- src/no_sleep/run.py
+++ src/no_sleep/run.py
@@ -1,33 +1,28 @@
 import tkinter as tk
-from sys import argv
+from argparse import ArgumentParser
 
-from no_sleep.gui import ICON_FILE, NoSleepGui
+from no_sleep.gui import ICON_FILE, NoSleepGui, __version__
 
 
-def run(sys_tray: bool = False, start_min: bool = False) -> None:
-    """
-    Run the app.
+def run() -> None:
+    """Run the app."""
+    parser = ArgumentParser(description='no more sleep')
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s version: ' + __version__)
+    parser.add_argument('-t', '--tray', action='store_true', dest='tray', help='show tray icon')
+    parser.add_argument('-m', '--minimized', action='store_true', dest='minimized', help='start minimized')
+    args = parser.parse_args()
 
-    :param sys_tray:
-    :param start_min:
-    """
     root = tk.Tk()
     width, height = 220, 75
     root.geometry(f'{width}x{height}')
     root.minsize(width=width, height=height)
     root.iconbitmap(ICON_FILE)
     root.title('NoSleep')
-    NoSleepGui(master=root, sys_tray=sys_tray)
-    if start_min:
+    NoSleepGui(master=root, cli_args=args)
+    if args.minimized:
         root.withdraw()
     root.mainloop()
 
 
 if __name__ == '__main__':
-    tray = False
-    start_as_min = False
-    if 'tray' in argv:
-        tray = True
-    if 'min' in argv:
-        start_as_min = True
-    run(sys_tray=tray, start_min=start_as_min)
+    run()
